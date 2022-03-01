b625d4119a836b8f254fefae00be58b3c548c14e
numankilic <numankilic35@gmail.com>
2022-03-01 16:31:24 +0300

    Initial commit

---
+++ src/main/java/com/example/security_example/student/Student.java
@@ -0,0 +1,10 @@
+package com.example.security_example.student;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+}
